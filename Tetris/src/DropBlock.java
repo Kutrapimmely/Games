c58cf58146bc2acee06022f7a43cc06a7e3f5f78
Kutrapimmely <matthiashinterleitner5@gmail.com>
2022-01-14 23:23:09 +0100

    Add new functions. Dropping blocks works now!

--- Tetris/src/DropBlock.java
+++ Tetris/src/DropBlock.java
@@ -1,9 +1,43 @@
 
-public class DropBlock implements Runnable{
+public class DropBlock extends Thread{
     @Override
     public void run() {
-        int counter = 0;
 
+        while(Tetris.checkIfGameOver() == false){
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            for (int i = 0; i < Tetris.playfield.length - 1; i++) {
+                for (int j = 0; j < Tetris.playfield[i].length; j++) {
+                    if(Tetris.playfield[i][j] == 2){
+                        Tetris.playfield[i + 1][j] = 3;
+                        Tetris.playfield[i][j] = 0;
+                    }
 
+                }
+            }
+            for (int i = 0; i < Tetris.playfield.length - 1; i++) {
+                for (int j = 0; j < Tetris.playfield[i].length; j++) {
+                    if(Tetris.playfield[i][j] == 3){
+                        Tetris.playfield[i][j] = 2;
+                    }
+                }
+            }
+
+            Tetris.myDraw.clear();
+            Tetris.drawPlayfield();
+            Tetris.makeBlockSolid();
+            if(Tetris.checkIfRowsFull() >= 0){
+                try{
+                    Thread.sleep(1000);
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+                Tetris.dropRow(Tetris.checkIfRowsFull());
+            }
+
+        }
     }
 }
--- Tetris/src/Tetris.java
+++ Tetris/src/Tetris.java
@@ -10,24 +10,28 @@ import codedraw.textformat.VerticalAlign;
 
 import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Tetris {
 
     public static int playfield[][];
     public static CodeDraw myDraw;
+    public static int currentBlock;
+    public static int currentRotate;
     public static int currentBlockPosition[][];
 
-    public static void drawPlayfield(){
+    public static void drawPlayfield() {
         myDraw.setColor(Color.BLUE);
         for (int i = 1; i < playfield.length; i++) {
-            for (int j = 1; j < playfield[i].length; j++) {
-                if(playfield[i][j] == 1){
+            for (int j = 0; j < playfield[i].length; j++) {
+                if (playfield[i][j] == 1) {
                     myDraw.setColor(Color.BLUE);
                     myDraw.fillSquare(j * 40, i * 40, 40);
-                }
-                else if(playfield[i][j] == 2){
+                } else if (playfield[i][j] == 2) {
                     myDraw.setColor(Color.RED);
                     myDraw.fillSquare(j * 40, i * 40, 40);
                 }
@@ -36,99 +40,134 @@ public class Tetris {
 
         myDraw.setColor(Color.BLACK);
         for (int i = 0; i < playfield.length - 1; i++) {
-            myDraw.drawLine(0,i * 40, 400, i * 40);
+            myDraw.drawLine(0, i * 40, 400, i * 40);
 
         }
         for (int i = 0; i < playfield[0].length; i++) {
-            myDraw.drawLine(i * 40,0, i * 40, 800);
+            myDraw.drawLine(i * 40, 0, i * 40, 800);
         }
 
         myDraw.show();
     }
 
-    public static boolean checkIfBlockReachedBottom(){
-        if(currentBlockPosition[0][0] == 0 || playfield[currentBlockPosition[0][0] - 1][currentBlockPosition[0][1]] == 1){
-            return true;
+    public static boolean checkIfBlockReachedBottom() {
+
+        int x = 0;
+        int y = 0;
+        LinkedList<Integer> list = getPositionFromCurrentBlock();
+
+        while(!(list.isEmpty())){
+            y = list.pop();
+            x = list.pop();
+            if(x == playfield.length - 2 || playfield[x + 1][y] == 1){
+                return true;
+            }
         }
-        if(currentBlockPosition[1][0] == 0 || playfield[currentBlockPosition[1][0] - 1][currentBlockPosition[1][1]] == 1){
-            return true;
-        }
-        if(currentBlockPosition[2][0] == 0 || playfield[currentBlockPosition[2][0] - 1][currentBlockPosition[2][1]] == 1){
-            return true;
-        }
-        if(currentBlockPosition[3][0] == 0 || playfield[currentBlockPosition[3][0] - 1][currentBlockPosition[3][1]] == 1){
-            return true;
-        }
+
         return false;
     }
-
-    public static int genNextBlock(){
-        return (int)Math.random() % 7;
+    public static LinkedList<Integer> getPositionFromCurrentBlock(){
+        LinkedList<Integer> list = new LinkedList();
+        for (int i = 0; i < playfield.length; i++) {
+            for (int j = 0; j < playfield[i].length; j++) {
+                if(playfield[i][j] == 2){
+                    list.push(i);
+                    list.push(j);
+                }
+            }
+        }
+        return list;
     }
-    public static void putBlockIntoPlayField(){
-        switch(genNextBlock()){
+
+    public static void rotateBlock(int inputAsASCII) {
+
+    }
+
+    public static int genNextBlock() {
+        return (int) (Math.random() * 7);
+    }
+
+    public static void putBlockIntoPlayField() {
+        currentBlock = genNextBlock();
+
+        switch (currentBlock) {
             case 0:                                                     //----
-                playfield[playfield.length - 1][4] = 2;
-                playfield[playfield.length - 1][5] = 2;
-                playfield[playfield.length - 1][6] = 2;
-                playfield[playfield.length - 1][7] = 2;
+                playfield[0][4] = 2;
+                playfield[0][5] = 2;
+                playfield[0][6] = 2;
+                playfield[0][7] = 2;
                 break;
             case 1:
-                playfield[playfield.length - 2][3] = 2;                 //-
-                playfield[playfield.length - 2][4] = 2;                 //---
-                playfield[playfield.length - 2][5] = 2;
-                playfield[playfield.length - 1][3] = 2;
+                playfield[1][3] = 2;                 //-
+                playfield[1][4] = 2;                 //---
+                playfield[1][5] = 2;
+                playfield[0][3] = 2;
                 break;
             case 2:
-                playfield[playfield.length - 2][3] = 2;                 //  -
-                playfield[playfield.length - 2][4] = 2;                 //---
-                playfield[playfield.length - 2][5] = 2;
-                playfield[playfield.length - 1][5] = 2;
+                playfield[1][3] = 2;                 //  -
+                playfield[1][4] = 2;                 //---
+                playfield[1][5] = 2;
+                playfield[0][5] = 2;
                 break;
             case 3:
-                playfield[playfield.length - 2][3] = 2;                 //--
-                playfield[playfield.length - 2][4] = 2;                 //--
-                playfield[playfield.length - 1][3] = 2;
-                playfield[playfield.length - 1][4] = 2;
+                playfield[1][3] = 2;                 //--
+                playfield[1][4] = 2;                 //--
+                playfield[0][3] = 2;
+                playfield[0][4] = 2;
                 break;
             case 4:
-                playfield[playfield.length - 2][3] = 2;                 // --
-                playfield[playfield.length - 2][4] = 2;                 //--
-                playfield[playfield.length - 1][4] = 2;
-                playfield[playfield.length - 1][5] = 2;
+                playfield[1][3] = 2;                 // --
+                playfield[1][4] = 2;                 //--
+                playfield[0][4] = 2;
+                playfield[0][5] = 2;
                 break;
             case 5:
-                playfield[playfield.length - 2][4] = 2;                 //--
-                playfield[playfield.length - 2][5] = 2;                 // --
-                playfield[playfield.length - 1][3] = 2;
-                playfield[playfield.length - 1][4] = 2;
+                playfield[1][4] = 2;                 //--
+                playfield[1][5] = 2;                 // --
+                playfield[0][3] = 2;
+                playfield[0][4] = 2;
                 break;
             case 6:
-                playfield[playfield.length - 2][3] = 2;                 // -
-                playfield[playfield.length - 2][4] = 2;                 //---
-                playfield[playfield.length - 1][5] = 2;
-                playfield[playfield.length - 1][4] = 2;
+                playfield[1][3] = 2;                 // -
+                playfield[1][4] = 2;                 //---
+                playfield[0][5] = 2;
+                playfield[0][4] = 2;
                 break;
         }
     }
 
-    public static int checkIfRowsFull(){
+    public static int checkIfRowsFull() {
 
         for (int i = 0; i < playfield.length; i++) {
             int help = 0;
             for (int j = 0; j < playfield[i].length; j++) {
-                if(playfield[i][j] == 1){
+                if (playfield[i][j] == 1) {
                     help++;
                 }
             }
-            if(help == 10){
+            if (help == 10) {
                 return i;
             }
         }
         return -1;
     }
+    public static void makeBlockSolid(){
 
-    public static void dropRow(int row){
+        if(checkIfBlockReachedBottom() == true){
+            int x = 0;
+            int y = 0;
+            LinkedList<Integer> list = getPositionFromCurrentBlock();
+
+            while(!(list.isEmpty())){
+                y = list.pop();
+                x = list.pop();
+                playfield[x][y] = 1;
+            }
+            Tetris.putBlockIntoPlayField();
+        }
+    }
+
+    public static void dropRow(int row) {
         playfield[row] = new int[10];
         int temp[];
         for (int i = row; i < playfield.length - 2; i++) {
@@ -137,8 +176,8 @@ public class Tetris {
         }
     }
 
-    public static boolean checkIfGameOver(){
-        if( IntStream.of(playfield[playfield.length- 1]).anyMatch(x -> x == 1)){
+    public static boolean checkIfGameOver() {
+        if (IntStream.of(playfield[1]).anyMatch(x -> x == 1)) {
             return true;
         }
         return false;
@@ -148,15 +187,22 @@ public class Tetris {
     public static void main(String[] args) {
         playfield = new int[21][10];
         myDraw = new CodeDraw(400, 800);
+        playfield[1][0] = 2;
+        playfield[1][1] = 2;
+        playfield[1][2] = 2;
+        playfield[1][3] = 2;
+
+
         playfield[0][0] = 1;
         playfield[19][1] = 1;
         playfield[19][2] = 1;
         playfield[19][3] = 1;
+
         drawPlayfield();
+        DropBlock dropBlock = new DropBlock();
+
+        dropBlock.start();
 
     }
 
 }
-
-
-
